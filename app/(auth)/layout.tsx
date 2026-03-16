0d75f5593f9b322c145fccec2c0ee33e1cf4ddd5
ninazmul <nazmulsaw@gmail.com>
2026-03-16 15:45:47 +0600

    Auth layout updated with header and footer

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,47 @@
+"use client";
+
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+import ScrollHeaderWrapper from "@/components/shared/ScrollHeaderWrapper";
+import SearchDrawer from "@/components/shared/SearchDrawer";
+import { useLayoutEffect, useRef, useState } from "react";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const headerRef = useRef<HTMLDivElement>(null);
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const [searchOpen, setSearchOpen] = useState(false);
+
+  useLayoutEffect(() => {
+    if (headerRef.current) {
+      const resizeObserver = new ResizeObserver(() => {
+        setHeaderHeight(headerRef.current!.offsetHeight);
+      });
+
+      resizeObserver.observe(headerRef.current);
+      setHeaderHeight(headerRef.current.offsetHeight);
+
+      return () => resizeObserver.disconnect();
+    }
+  }, []);
   return (
-    <div className="flex items-center justify-center min-h-screen w-full bg-primary-50 bg-cover bg-fixed bg-center">
-      {children}
+    <div className="flex h-screen flex-col">
+      <ScrollHeaderWrapper>
+        <div ref={headerRef}>
+          <Header openSearch={() => setSearchOpen(true)} />
+        </div>
+      </ScrollHeaderWrapper>
+      <main
+        style={{ paddingTop: headerHeight }}
+        className="flex items-center justify-center my-12 md:my-16"
+      >
+        {children}
+      </main>
+      <SearchDrawer
+        open={searchOpen}
+        onOpenChange={setSearchOpen}
+        headerHeight={headerHeight}
+      />
+      <Footer />
     </div>
   );
 };
