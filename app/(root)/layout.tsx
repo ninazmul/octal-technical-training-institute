0ece594f692eec8d21d7378543ecf0ab1e407f34
ninazmul <nazmulsaw@gmail.com>
2026-03-25 11:52:38 +0600

    tawk.to added on root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,6 +7,7 @@ import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 import SearchDrawer from "@/components/shared/SearchDrawer";
 import BengaliFontDetector from "@/components/shared/BengaliFontDetector";
+import Script from "next/script";
 // import SearchDrawer from "@/components/shared/SearchDrawer";
 
 export default function RootLayout({
@@ -52,6 +53,24 @@ export default function RootLayout({
       />
 
       <Footer />
+
+      <Script
+        id="tawk-to"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/69c230d629e9681c3d64df82/1jkf8tirp';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+      })();
+    `,
+        }}
+      />
     </div>
   );
 }
